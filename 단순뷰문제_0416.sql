3b965de3d1b93601560e303acd20735295467f3c
moonwg09 <moonwg09@naver.com>
2026-04-16 18:57:17 +0900

    데이터베이스 문제

--- 단순뷰문제_0416.sql
+++ 단순뷰문제_0416.sql
@@ -229,10 +229,10 @@ create view v_book_price_stats as
 
 
 [사원 데이터베이스]
-Dept: 부서(depno NUMBER(2), dname VARCHAR(14), loc VARCHAR(13))
-Emp : 사원(empno NUMBER(4), ename VARCHAR(10), job(업무) VARCHAR(9),
-      mgr(팀장번호) NUMBER(4), hiredate DATE, sal NUMBER(7,2), 
-      comm(커미션금액) NUMBER(7,2), deptno NUMBER(2))
+Employee(empno, name, phoneno, address, sex, position, depno)
+Department(depno, deptname, ,manager)
+Project(projno, projname, deptno)
+Works(empno, projno, hours_worked)
 
 문제 1.
 모든 사원의 사원번호, 이름, 부서명, 직급, 급여를 보여주는 뷰 v_emp_basic을 작성하시오.
@@ -244,72 +244,195 @@ create view v_emp_basic as
 
 문제 2.
 급여가 500만원 이상인 사원의 이름, 직급, 급여, 부서명을 보여주는 읽기 전용 뷰 v_high_salary_emp를 작성하시오.
-create view v_high_salary_emp as
+create or replace view v_high_salary_emp as
     select e.ename, e.job, e.sal, d.dname
     from emp e
     join dept d on e.deptno = d.deptno
-    where 
+    where e.sal > 500
+with read only;
 
 
 문제 3.
 현재 진행 중인 프로젝트(오늘 날짜가 시작일과 종료일 사이)의 프로젝트번호, 프로젝트명, 시작일, 종료일을 보여주는 뷰 v_active_projects를 작성하시오.
+create or replace view v_active_projects as
+    select projno, projname, start_date, end_date
+    from projects
+    where sysdate between start_date and end_date;
 
 문제 4.
 입사일이 2019년 이전인 사원의 사원번호, 이름, 부서명, 입사일, 근속연수를 보여주는 뷰 v_veteran_employee를 작성하시오.
 (근속연수는 현재 연도 기준으로 계산)
+create or replace view v_veteran_employee as
+    select e.empno, e.ename, d.dname, e.hiderdate, EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM e.hiredate) AS work_years
+    from emp e
+    join dept d on d.deptno = e.deptno
+    where e.hiredate < TO_DATE('2019-01-01', 'YYYY-MM-DD');
 
 문제 5.
 부서 위치가 '서울'인 부서의 부서번호, 부서명, 예산을 보여주는 뷰 v_seoul_department를 작성하시오.
+create or replace view v_seoul_department as
+    select deptno, dname, budget
+    from dept
+    where loc = '서울';
 
 문제 6.
 직급이 '부장' 또는 '이사'인 사원의 사원번호, 이름, 직급, 급여를 보여주는 읽기 전용 뷰 v_senior_position를 작성하시오.
+create or replace view v_senior_position as
+    select empno, ename, job, sal
+    from emp
+    where job in ('부장', '이사')
+with read ONLY;
 
 문제 7.
 프로젝트에서 'PM' 역할을 담당하는 사원의 사원번호, 프로젝트번호, 담당역할, 투입시간을 보여주는 뷰 v_pm_role을 작성하시오.
+create or replace view v_pm_role as
+    select empno, projno, role, work_hours
+    from project_assignments
+    where role = 'PM';
 
 문제 8.
 급여가 300만원 미만인 사원 중 입사일이 2022년 이후인 사원의 이름, 직급, 급여, 입사일을 보여주는 뷰 v_junior_emp를 작성하시오.
+create or replace view v_junior_emp as
+    select ename, job, sal, hiredate
+    from emp
+    where sal < 300
+      and hiredate >= TO_DATE('2022-01-01', 'YYYY-MM-DD');
+
 
 문제 9.
 예산이 1억원 이상인 프로젝트의 프로젝트명, 시작일, 종료일, 예산을 보여주는 읽기 전용 뷰 v_large_budget_project를 작성하시오.
+create or replace view v_large_budget_project as
+    select p.projname, p.start_date, p.end_date, d.budget
+    from dept d
+    join projects p on p.deptno = d.deptno
+    where d.budget >= 10000
+with read only;
+
 
 문제 10.
 관리자가 없는 최상위 관리자이면서 급여가 700만원 이상인 사원의사원번호, 이름, 부서명, 직급, 급여를 보여주는 읽기 전용 뷰 v_top_executive를 작성하시오
+create or replace view v_top_executive as
+    select e.empno, e.ename, d.dname, e.job, e.sal
+    from emp e
+    join dept d on d.deptno = e.deptno
+    where e.sal >= 700
+    and e.mgr is null
+with read only;
 
 
 [복합 뷰]
 문제 1.
 부서별 평균 급여와 최고 급여, 최저 급여를 보여주는 뷰 v_dept_salary_stats를 작성하시오.
 (출력 컬럼: 부서명, 평균급여, 최고급여, 최저급여)
+create view v_dept_salary_stats as
+    select d.dname, avg(e.sal) as '평균급여', max(e.sal) as '최고급여', min(e.sal) as '최저급여'
+    from dept d
+    join emp e e.deptno = d.deptno
+    group by d.dname;
 
 문제 2.
 각 사원이 참여한 프로젝트 수와 총 투입시간을 보여주는 뷰 v_emp_project_summary를 작성하시오.
 (출력 컬럼: 사원이름, 참여프로젝트수, 총투입시간)
+create or replace view v_emp_project_summary as
+    select 
+        e.ename as 사원이름, 
+        count(w.projno) as 참여프로젝트수, 
+        sum(w.hours_worked) as 총투입시간
+    from emp e 
+    join works w on e.empno = w.empno
+    group by e.ename;
 
 문제 3.
 부서별 예산 대비 해당 부서 사원들의 평균 급여 비율을 보여주는 뷰 v_dept_budget_ratio를 작성하시오.
 (출력 컬럼: 부서명, 부서예산, 평균급여, 급여예산비율)
 단, 급여예산비율은 소수점 2자리까지 표시한다.
+create or replace view v_dept_budget_ratio as
+    select d.dname, d.sal, avg(e.sal) as "평균급여", round(avg(e.sal) / d.budget * 100, 2)
+    from emp e
+    join dept d on d.deptno = e.deptno
+    group by d.dname, d.budget;
 
 문제 4.
 현재 진행 중인 프로젝트에 참여하고 있는 사원의 이름, 부서명, 프로젝트명, 담당역할을 보여주는 뷰 v_active_project_emp를 작성하시오.
+create or replace view v_active_project_emp as
+    select e.ename, d.dname, p.projname, w.role
+    from emp e
+    join dept d on e.deptno = d.deptno
+    join works w on e.empno = w.empno
+    join projects p on w.projno = p.projno
+    where sysdate between p.start_date and p.end_date;
 
 문제 5.
 한 번도 프로젝트에 참여하지 않은 사원의 사원번호, 이름, 부서명, 직급을 보여주는 뷰 v_no_project_emp를 작성하시오.
+create or replace view v_no_project_emp as
+    select e.empno, e.ename, d.dname, e.job
+    from emp e
+    join dept d on d.deptno= e.deptno
+    where not exists(
+        select 1
+        from work w
+        where w.empno = e.empno
+    );
+
 
 문제 6.
 프로젝트별 참여 사원 수와 총 투입시간, 평균 투입시간을 보여주는 뷰 v_project_stats를 작성하시오.
 (출력 컬럼: 프로젝트명, 참여사원수, 총투입시간, 평균투입시간)
+create or replace view v_project_stats as
+    select p.projname, count(e.empno) as '참여사원수', sum(w.hours_worked) as '총투입시간', avg(w.hours_worked) as '평균투입시간'
+    from emp e
+    join works w on w.empno = e.empno
+    join projects p on p.projno = w.projno
+    group by p.projname;
+
 
 문제 7.
 자신이 속한 부서의 평균 급여보다 높은 급여를 받는 사원의 이름, 부서명, 급여, 부서평균급여를 보여주는 뷰 v_above_dept_avg를 작성하시오.
+create or replace view v_above_dept_avg as
+    select
+        e.ename, 
+        d.dname, 
+        e.sal, 
+        dept_avg.avg_sal as 부서평균급여
+    from emp e
+    join dept d on e.deptno = d.deptno
+    join (
+        select deptno, avg(sal) as avg_sal
+        from emp
+        GROUP BY deptno
+    ) dept_avg on e.deptno = dept_avg.deptno
+    where e.sal > dept_avg.avg_sal;
+
 
 문제 8.
 각 부서에서 가장 오래 근무한 사원의 이름, 부서명, 입사일, 근속연수를 보여주는 뷰 v_longest_serving를 작성하시오.
+create or replace view v_longest_serving as
+    select e.ename, d.dname, e.hiderdate, EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM e.hiredate) AS work_years
+    from emp e
+    join dept d on e.deptno = d.deptno
+    where (e.deptno, e.hiredate) in (
+        select deptno, min(hiredate)
+        from emp
+        group by deptno
+    );
 
 문제 9.
 2개 이상의 프로젝트에 참여하면서 총 투입시간이 100시간 이상인 사원의 이름, 참여프로젝트수, 총투입시간을 보여주는 뷰 v_active_emp를 작성하시오.
+create or replace view v_active_emp
+    select e.ename, count(p.projno) as "참여프로젝트수", sum(w.hours_worked) as '총투입시간'
+    from emp e
+    join works w on w.empno = e.empno
+    group by e.name
+    having count(p.projno) >= 2 and
+    sum(w.hours_worked) >= 100;
+
 
 문제 10.
 부서별로 'PM' 역할을 맡은 사원 수와 해당 부서의 평균 급여를 보여주는 뷰 v_dept_pm_stats를 작성하시오.
 (출력 컬럼: 부서명, PM수, 부서평균급여)
+create or replace view v_dept_pm_stats as
+    select d.dname, COUNT(CASE WHEN w.role = 'PM' THEN 1 END) AS PM수, avg(e.sal) as "부서평균급여"
+    from emp e
+    join dept d on d.deptno = e.deptno
+    LEFT JOIN works w ON e.empno = w.empno
+    group by d.dname; 
